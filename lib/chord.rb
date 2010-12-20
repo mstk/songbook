90ee95b4988ef0835cce1fd38c40f264e165bf9d
mstk <mstksg@gmail.com>
2010-12-20 03:31:39 -0800

    chord rendering set-up

---
+++ lib/chord.rb
@@ -0,0 +1,60 @@
+# Represents a single Chord, stored as a number and a mode (:major or :minor).  Not actually
+#   stored in the database, but rather used as a helper class for ChordProgression to help render 
+#   chords into a given key in the proper color.
+# Chord instances are shared across ChordProgression objects (there are only 14 possible,
+#   currently), in a sort of singleton pattern.  In a way, they represent constants and are 
+#   immutable.  To access the Chord instance for a given chord, use Chord::CHORD(chord symbol)
+#
+class Chord
+  
+  private_class_method :new
+  
+  @@roman_numerals = %w[ I II III IV V VI VII ]
+  @@chord_symbols = { :major => @@roman_numerals.map { |c| c.downcase.intern },
+                      :minor => @@roman_numerals.map { |c| c.intern } }
+  CHORD_SYMBOLS = @@chord_symbols.values.flatten
+  
+  @@CHORD_INDEX = Hash.new do |h,c|
+    new(c)
+  end
+  
+  # Initialize a new Chord instance with a given chord_symbol.  Private method, and should only be
+  # done by @@CHORD_INDEX hash lambda, to maintain singleton pattern.
+  #
+  # @param [Symbol] chord_symbol Symbol of the chord to convert.
+  #
+  def initialize(chord_symbol)
+    raise ArgumentError unless CHORD_SYMBOLS.include?(chord_symbol)
+    
+    @step = @@roman_numerals.index(chord_symbol.to_s.upcase.intern)
+    @mode = [:major,:minor].find { |mode| mode.include? chord_symbol}
+  end
+  
+  # Renders the chord to the given key, with the given color scheme.
+  # 
+  # @param [SongKey] SongKey of the key to render the chord into.
+  # @param [ColorScheme] ColorScheme to follow.  See ColorScheme for more info.  Optional -- 
+  #   defaults to a default color scheme.
+  # @return [Symbol] An absolute_chord symbol corresponding to the chord rendered to the given 
+  #   SongKey and given ColorScheme.
+  # 
+  def render(key,color_scheme = ColorScheme.get('default'))
+    # oh my
+  end
+  
+  # Retrieve the Chord instance for a given chord_symbol.
+  #
+  # @param [Symbol] chord_symbol A key symbol from Chord::CHORD_SYMBOLS.
+  # @return [Chord] SongKey corresponding to that chord symbol
+  #
+  def Chord.CHORD(chord_symbol)
+    raise ArgumentError unless CHORD_SYMBOLS.include?(chord_symbol)
+    
+    @@CHORD_INDEX[chord_symbol]
+  end
+  
+  def symbol
+    @@chord_symbols[@mode][@step]
+  end
+  
+end
--- lib/chord_progression.rb
+++ lib/chord_progression.rb
@@ -9,6 +9,29 @@
 #
 class ChordProgression
   
+  # Render the ChordProgression to the given key and color scheme.
+  # 
+  # @param [SongKey] SongKey of the key to render the chord progression into.
+  # @param [ColorScheme] ColorScheme to follow.  See ColorScheme for more info.  Optional -- 
+  #   defaults to a default color scheme.
+  # @return [Array<Symbol>] Array of absolute_chord symbols corresponding to the stored progression
+  #   rendered to the given SongKey and given ColorScheme.
+  # 
+  def render(key,color_scheme = ColorScheme.get('default'))
+    chords_array.map { |c| c.render(key,color_scheme) }
+  end
   
+  private
+  
+  # Access the array of Chord instances corresponding with the array of chord_symbols stored in the
+  #   database.
+  #
+  # @return [Array<Chord>] Array of Chord instances corresponding with the array of chord_symbols
+  #   stored in the database.
+  # 
+  def chords_array
+    @chords_array = progression.map { |c| Chord.CHORD(c) } unless @chords_array
+    @chords_array
+  end
   
 end
