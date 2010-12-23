df6042a9bceea691919b7a77a9022738b5c0a74e
Justin Le <mstksg@gmail.com>
2010-12-23 02:22:21 -0800

    added support for different modes. should start speccing this.

--- lib/scale_generator.rb
+++ lib/scale_generator.rb
@@ -35,32 +35,60 @@ module ScaleGenerator
   
   NATURALS.freeze
   
+  MODE_SHIFT = { :major       =>   0,
+                 :minor       =>  -9,
+                 :ionian      =>   0,
+                 :dorian      =>  -2,
+                 :phrygian    =>  -4,
+                 :lydian      =>  -5,
+                 :mixolydian  =>  -7,
+                 :aeolian     =>  -9,
+                 :locrian     => -11 }
+  
   # Generates the scale for a given key and given color.
   # 
   # @param [SongKey] song_key
   #   A SongKey instance to render the scale into.
   # @param [Symbol] color
   #   `:sharp` or `:flat` -- the accidental mode/color to render the scale into.
+  # @param [Symbol] mode
+  #   The seven modern musical modes.  `:major` and `:minor` can be used as shorthand for `:ionian` 
+  #   and `:aeolian`, respectively.
   # @return [Array<Symbol>]
   #   An array with the symbol of the absolute note value for each step.
   #
-  def ScaleGenerator.generate_scale(song_key, color = :flat)
-    # initialize array of accidentals
-    scale_accidentals = Array.new(7) { 0 }
-    
-    # get the id of the "natural" of the key, clean of accidentals.
-    root_natural = song_key.symbol(ColorScheme.get_scheme_all(color)).to_s[0].intern
-    natural_id = NATURALS.index(root_natural)
-    
-    # populate the array of accidentals
-    number_of_accidentals = CIRCLE_OF_FIFTHS[color].index(song_key.key_id)
-    number_of_accidentals.times { |n| scale_accidentals[ACCIDENTALS[color][n]] += COLOR_VALUE[color] }
-    
-    # make sure there aren't too many accidentals
-    raise "Something is wrong with this scale." if scale_accidentals.any? { |n| n.abs > 3 }
-    
-    # map scale accidentals to actual scale, by steps
-    return (0..6).map { |step| "#{NATURALS[(natural_id + step) % 7]}#{COLOR_STRING[color] * scale_accidentals[step].abs}".intern }
+  def ScaleGenerator.generate_scale(song_key, color = :flat, mode = :major)
+    if MODE_SHIFT[mode] == 0
+      
+      # initialize array of accidentals
+      scale_accidentals = Array.new(7) { 0 }
+      
+      # get the id of the "natural" of the key, clean of accidentals.
+      root_natural = song_key.symbol(ColorScheme.get_scheme_all(color)).to_s[0].intern
+      natural_id = NATURALS.index(root_natural)
+      
+      # populate the array of accidentals
+      number_of_accidentals = CIRCLE_OF_FIFTHS[color].index(song_key.key_id)
+      number_of_accidentals.times { |n| scale_accidentals[ACCIDENTALS[color][n]] += COLOR_VALUE[color] }
+      
+      # make sure there aren't too many accidentals
+      raise "Something is wrong with this scale." if scale_accidentals.any? { |n| n.abs > 3 }
+      
+      # map scale accidentals to actual scale, by steps
+      return (0..6).map { |step| "#{NATURALS[(natural_id + step) % 7]}#{COLOR_STRING[color] * scale_accidentals[step].abs}".intern }
+      
+    else
+      
+      scale = ScaleGenerator.generate_scale(song_key.transpose(MODE_SHIFT[mode]),color,:major)
+      
+      # magical math function that gets the interval-1 of a mode from the given pitch shift
+      # only works up to 11, but that's all we need, isn't it?
+      steps = (MODE_SHIFT[mode].abs+1)/2
+      
+      # rotate the scale by the number of steps
+      return scale[steps..-1] + scale[0..(steps-1)]
+      
+    end
   end
   
 end
